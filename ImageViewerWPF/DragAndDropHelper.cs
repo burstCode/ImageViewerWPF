1f0fcdaf780922229487c7e17b9dbff3956bf748
burstlimit <yourewelcomex@gmail.com>
2025-04-19 16:05:09 +0300

    Добавьте файлы проекта.

---
+++ ImageViewerWPF/DragAndDropHelper.cs
@@ -0,0 +1,92 @@
+﻿using System.Windows;
+
+namespace ImageViewerWPF
+{
+    public static class DragAndDropHelper
+    {
+        public static readonly DependencyProperty IsDragOverProperty =
+            DependencyProperty.RegisterAttached("IsDragOver", typeof(bool), typeof(DragAndDropHelper),
+                new PropertyMetadata(false));
+
+        public static bool GetIsDragOver(DependencyObject obj) => (bool)obj.GetValue(IsDragOverProperty);
+        public static void SetIsDragOver(DependencyObject obj, bool value) => obj.SetValue(IsDragOverProperty, value);
+
+        public static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.RegisterAttached("IsEnabled", typeof(bool), typeof(DragAndDropHelper),
+                new PropertyMetadata(false, OnIsEnabledChanged));
+
+        public static void OnIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element)
+            {
+                if ((bool)e.NewValue)
+                {
+                    element.DragOver += OnDragOver;
+                    element.DragLeave += OnDragLeave;
+                    element.Drop += OnDrop;
+                }
+                else
+                {
+                    element.DragOver -= OnDragOver;
+                    element.DragLeave -= OnDragLeave;
+                    element.Drop -= OnDrop;
+                }
+            }
+        }
+
+        public static void OnDragOver(object sender, DragEventArgs e)
+        {
+            if (sender is DependencyObject d)
+            {
+                SetIsDragOver(d, true);
+            }
+
+            if (IsValidDragData(e.Data))
+            {
+                e.Effects = DragDropEffects.Copy;
+                e.Handled = true;
+            }
+        }
+
+        public static void OnDragLeave(object sender, DragEventArgs e)
+        {
+            if (sender is DependencyObject d)
+            {
+                SetIsDragOver(d, false);
+            }
+        }
+
+        public static void OnDrop(object sender, DragEventArgs e)
+        {
+            if (sender is DependencyObject d)
+            {
+                SetIsDragOver(d, false);
+            }
+
+            if (IsValidDragData(e.Data))
+            {
+                HandleDrop(e.Data);
+                e.Handled = true;
+            }
+        }
+
+        public static bool IsValidDragData(IDataObject data)
+        {
+            return data.GetDataPresent(DataFormats.FileDrop) ||
+                   data.GetDataPresent(DataFormats.Bitmap);
+        }
+
+        public static void HandleDrop(IDataObject data)
+        {
+            // Ваша логика обработки перетаскивания
+            if (data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])data.GetData(DataFormats.FileDrop);
+                // Обработка файлов и папок
+            }
+        }
+
+        public static bool GetIsEnabled(DependencyObject obj) => (bool)obj.GetValue(IsEnabledProperty);
+        public static void SetIsEnabled(DependencyObject obj, bool value) => obj.SetValue(IsEnabledProperty, value);
+    }
+}
---
+++ ImageViewerWPF/StartWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace ImageViewerWPF
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            base.OnDragEnter(e);
+            if (DragAndDropHelper.IsValidDragData(e.Data))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnDrop(DragEventArgs e)
+        {
+            base.OnDrop(e);
+            if (DragAndDropHelper.IsValidDragData(e.Data))
+            {
+                // Ваша логика обработки сброса файлов
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                ProcessDroppedFiles(files);
+            }
+            e.Handled = true;
+        }
+
+        private void ProcessDroppedFiles(string[] paths)
+        {
+            List<string> imageFiles = new List<string>();
+
+            foreach (string path in paths)
+            {
+                if (File.Exists(path))
+                {
+                    if (IsImageFile(path))
+                        imageFiles.Add(path);
+                }
+                else if (Directory.Exists(path))
+                {
+                    imageFiles.AddRange(GetImageFilesFromDirectory(path));
+                }
+            }
+
+            if (imageFiles.Count > 0)
+            {
+                // Открываем просмотрщик с найденными изображениями
+                //OpenViewer(imageFiles);
+            }
+        }
+
+        private bool IsImageFile(string filePath)
+        {
+            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".bmp", ".gif" };
+            string extension = Path.GetExtension(filePath).ToLower();
+            return imageExtensions.Contains(extension);
+        }
+
+        private IEnumerable<string> GetImageFilesFromDirectory(string directoryPath)
+        {
+            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".bmp", ".gif" };
+            return Directory.EnumerateFiles(directoryPath, "*.*", SearchOption.AllDirectories)
+                           .Where(f => imageExtensions.Contains(Path.GetExtension(f).ToLower()));
+        }
+    }
+}
